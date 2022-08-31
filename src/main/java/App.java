63a4a79a3c28c3ebf003ccf3d2727d3f72fe1de7
tda78 <tda78@mail.ru>
2022-09-01 02:55:29 +0700

    pp212 v1.0

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,17 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld hw1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld hw2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(hw1 == hw2);
+        System.out.println(cat1 == cat2);
+
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,14 @@
+public class Cat {
+    private String hello;
+
+    public Cat() {
+    }
+
+    public String getHello() {
+        return hello;
+    }
+
+    public void setHello(String hello) {
+        this.hello = hello;
+    }
+}
